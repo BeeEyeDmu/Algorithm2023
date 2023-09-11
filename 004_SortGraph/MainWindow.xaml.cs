4d938c5a65924db3b64444b45a029df63a63852b
BeeEye Dmu <bikang@konyang.ac.kr>
2023-09-11 12:58:13 +0900

    Add files via upload

--- 004_SortGraph/MainWindow.xaml.cs
+++ 004_SortGraph/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -55,6 +56,103 @@ namespace _004_SortGraph
         l.StrokeThickness = 4;
         can.Children.Add(l);
       }
+    }
+
+    private void btnBubble_Click(object sender, RoutedEventArgs e)
+    {
+      BubbleSort();
+    }
+
+    private void BubbleSort()
+    {
+      for(int i=N-1; i>0; i--)
+        for(int j=0; j<i; j++)
+          if (a[j] > a[j+1])
+          {
+            int t = a[j];
+            a[j] = a[j + 1];
+            a[j + 1] = t;
+          }
+
+      Graph();
+    }
+
+    private void btnQuick_Click(object sender, RoutedEventArgs e)
+    {
+      QuicKSort(a, 0, N-1);
+      Graph();
+    }
+
+    private void QuicKSort(int[] a, int left, int right)
+    {
+      if(left < right)
+      {
+        int q = Partition(a, left, right);
+        QuicKSort(a, left, q - 1);
+        QuicKSort(a, q + 1, right);
+      }
+    }
+
+    private int Partition(int[] a, int left, int right)
+    {
+      int i = left;
+      int j = right;
+
+      int pivot = a[left];
+
+      while(i < j)
+      {
+        while ((i <= right) && (a[i] < pivot))
+          i++;
+        while ((j >= left) && (a[j] > pivot))
+          j--;
+
+        if(i < j)
+        {
+          int t = a[i];
+          a[i] = a[j];
+          a[j] = t;
+
+          if (a[i] == a[j])
+            j--;
+        }
+      }
+
+      return j;
+    }
+
+    private void btnMerge_Click(object sender, RoutedEventArgs e)
+    {
+      MergeSort(a, 0, N-1);
+    }
+
+    private void MergeSort(int[] a, int left, int right)
+    {
+      if(left < right)
+      {
+        int mid = (left+right) / 2;
+        MergeSort(a, left, mid);
+        MergeSort(a, mid + 1, right);
+        Merge(a, left, mid, right);
+      }
+    }
+
+    int[] sorted = new int[MAX];
+    private void Merge(int[] a, int left, int mid, int right)
+    {
+      int i, j, k = left;
+      for (i = left, j = mid + 1; i <= mid && j <= right;)
+        sorted[k++] = (a[i] <= a[j]) ? a[i++] : a[j++];
+      if (i > mid)
+        for (int l = j; l <= right; l++)
+          sorted[k++] = a[l];
+      else
+        for (int l = i; l <= mid; l++)
+          sorted[k++] = a[l];
+
+      // sorted[]을 a[]로 복사
+      for (int l = left; l <= right; l++)
+        a[l] = sorted[l];
     }
   }
 }
